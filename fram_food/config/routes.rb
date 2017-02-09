f5ee0364aa62692f1aa05d8505a8dd310d67f725
Ringo <nd.chien.it@gmail.com>
2017-02-09 17:04:35 +0700

    Finish Sign up

--- fram_food/config/routes.rb
+++ fram_food/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "pages#show", page: "home"
   get "/pages/*page", to: "pages#show"
   get "/manage_pages/*page", to: "manage_pages#show"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: [:show]
 end
--- fram_food/db/schema.rb
+++ fram_food/db/schema.rb
@@ -10,17 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207071225) do
+ActiveRecord::Schema.define(version: 20170208053324) do
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.string   "serial"
+    t.float    "blance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "discount_products", force: :cascade do |t|
+    t.integer  "id_discount"
+    t.integer  "id_product"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.string   "description"
+    t.float    "value"
+    t.datetime "date_start"
+    t.datetime "date_end"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer  "unit_quantity"
+    t.float    "unit_price"
+    t.integer  "id_discount"
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.float    "total"
+    t.integer  "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.integer  "quantity"
+    t.float    "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_ratings_on_product_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "addres"
-    t.boolean  "admin"
+    t.boolean  "is_admin",        default: false
     t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
